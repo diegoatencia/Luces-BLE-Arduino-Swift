40d8f126337423cb67e65e97bbb322f4711322a4
Diego Atencia <diegoatencia96@gmail.com>
2020-03-29 19:13:26 -0300

    Se agregó pin 6 al código de Arduino

--- prueba_2/prueba_2.ino
+++ prueba_2/prueba_2.ino
@@ -4,11 +4,13 @@
 #define txPin 1
 
 int led = 9; //Defino pin que voy a usar del arduino
+int led2 = 6;
 int readInt;
 
 void setup() {
   Serial.begin(9600); //Activacion de puerto serial
   pinMode(led, OUTPUT); //Indica que el pin 9 (definido arriba), es un pin de salida
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
@@ -20,5 +22,6 @@ void loop() {
   
   Serial.println(readInt);
   analogWrite(led, readInt);
+  analogWrite(led2, readInt);
   
 }
